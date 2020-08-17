788ecf52a8f40b34347d643349264bb4617bec1a
Olena Mazurkiewicz <58334662+OlenaMazurkiewicz@users.noreply.github.com>
2020-08-17 12:25:42 +0300

    Update parser_Reddit.py

--- parser_Reddit.py
+++ parser_Reddit.py
@@ -5,26 +5,29 @@ import json
 
 async def request_data(url):
     async with aiohttp.request('GET', url) as resp:
-        await resp.read()
+        return await resp.read()
 
 
 async def get_reddit_top(subreddit):
     data_tot = await request_data(f'https://www.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=5')
     j = json.loads(data_tot)
-    for i in j['data']['name']:
+    for i in j['data']['children']:
+        reddit_name = i['data']['name']
+        score = i['data']['score']
         title = i['data']['title']
-        for k in i:
-            score = k['data']['score']
-            link = k['data']['url']
+        link = i['data']['url']
 
-
+    print(reddit_name + ':' + str(score) + title + ' (' + link + ')')
+#
 async def main():
+
     reddits = {
         "python",
         "compsci",
         "microbork"
     }
-    await asyncio.gather(*(get_reddit_top(subreddit) for subreddit in reddits))
-
-
+    data = await asyncio.gather(*(get_reddit_top(subreddit) for subreddit in reddits))
+    
+    
+    
 asyncio.run(main())
