3c4e8e9570cec275c9c856dd640b895dde50d74d
Olena Mazurkiewicz <58334662+OlenaMazurkiewicz@users.noreply.github.com>
2020-11-11 18:53:11 +0200

    Add files via upload

---
+++ Conn_Handler.py
@@ -0,0 +1,76 @@
+'''Создайте класс который будет хранить параметры для
+подключения к физическому юниту (например сервер). В своем
+списке атрибутов он должен иметь минимальный набор
+(unit_name, mac_address, ip_address, login, password).
+Вы должны описать каждый из этих атрибутов в виде гетеров и
+сеттеров (@property). У вас должна быть возможность
+получения и переопределения этих атрибутов в классе.'''
+
+
+class ConnHandler:
+    __slots__ = ['_unit_name', '_mac_address', '_ip_address', '_login', '_password']
+
+    def __init__(self, unit_name='', mac_address='', ip_address='', login='', password=''):
+        self._unit_name = unit_name
+        self._mac_address = mac_address
+        self._ip_address = ip_address
+        self._login = login
+        self._password = password
+
+    @property
+    def unit_name(self):
+        return self._unit_name
+
+    @unit_name.setter
+    def unit_name(self, value):
+        self._unit_name = value
+
+    @property
+    def mac_address(self):
+        return self._mac_address
+
+    @mac_address.setter
+    def mac_address(self, value):
+        self._mac_address = value
+
+    @property
+    def ip_address(self):
+        return self._ip_address
+
+    @ip_address.setter
+    def ip_address(self, value):
+        self._ip_address = value
+
+    @property
+    def login(self):
+        return self._login
+
+    @login.setter
+    def login(self, value):
+        self._login = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = value
+
+
+d = ConnHandler()
+
+
+d.unit_name = str('Marika')
+d.mac_address = 25
+d.ip_address = 10
+d.login = str('kva-kva')
+d.password = 19
+
+
+print(d.unit_name)
+print(d.mac_address)
+print(d.ip_address)
+print(d.login)
+print(d.password)
+
